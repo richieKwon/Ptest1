074ad16c1ba1f334eb75ee666314d4448212b923
richie <richie.kwon1@gmail.com>
2022-07-20 10:07:59 +0100

    singleton injection

--- Food.Data/IRestaurantData.cs
+++ Food.Data/IRestaurantData.cs
@@ -1,7 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using Food.Core;
+
 namespace Food.Data
 {
     public interface IRestaurantData
     {
-         
+        IEnumerable<Restaurant> GetAll();
     }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id = 1, Name = "YeraBBQ", Location = "CoventGarden", Cuisine = CuisineType.Mexican},
+                new Restaurant{Id = 2, Name = "JensKitchen", Location = "Wimbledon", Cuisine = CuisineType.Italian},
+                new Restaurant{Id = 3, Name = "ZeroDog", Location = "Soho", Cuisine = CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from item in restaurants
+                orderby item.Name  
+                select item;
+        }
+    }
+
+
 }
