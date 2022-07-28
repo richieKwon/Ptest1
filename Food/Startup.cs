9d645679ef560a954a2961df36330748deab4a77
richie <richie.kwon1@gmail.com>
2022-07-28 09:04:28 +0100

    datatable

--- Food/Startup.cs
+++ Food/Startup.cs
@@ -48,13 +48,13 @@ namespace Ptest1
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            }  
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            // app.UseMvc();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
